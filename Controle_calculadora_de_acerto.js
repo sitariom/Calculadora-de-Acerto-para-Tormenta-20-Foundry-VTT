d08f76668b78c4a4a09a282f767fd800071d054c
Sitariom <92954390+sitariom@users.noreply.github.com>
2024-09-28 03:25:40 -0300

    Update Controle_calculadora_de_acerto.js

--- Controle_calculadora_de_acerto.js
+++ Controle_calculadora_de_acerto.js
@@ -1,18 +1,1 @@
-// 🤖 Controle calculadora de acerto
-
-// Função para desativar a Calculadora de Acerto
-function desativarCalculadoraAcerto() {
-    if (typeof window.calculadoraAcertoAtiva !== 'undefined') {
-      window.calculadoraAcertoAtiva = false;
-      let indicador = document.getElementById('calculadoraAcertoIndicador');
-      if (indicador) {
-        indicador.innerHTML = '🤖 Calculadora de acerto desativada';
-      }
-      ui.notifications.info("Calculadora de acerto desativada.");
-    } else {
-      ui.notifications.warn("A macro Calculadora de acerto não está ativa ou não foi encontrada.");
-    }
-  }
-  
-  // Executar a função ao ativar esta macro
-  desativarCalculadoraAcerto();
+window.macroDesativarCalculadora();
